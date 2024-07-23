19ffba44051e41aba9246a8077917c679399baec
Reckless <reckless.xqy@gmail.com>
2024-07-23 18:33:50 +0800

    update: 新增部分预渲染 & 搜索和分页功能实现方式

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental', // 'incremental' 值允许您对特定路线采用 PPR(部分预渲染)
+  },
+};
 
 export default nextConfig;
